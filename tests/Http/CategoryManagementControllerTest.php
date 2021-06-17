a9108c872d8c2627b76b31ed2ccdce110f0c2232
christyjoshy <christyjoshy@users.noreply.github.com>
2021-06-17 18:02:18 +0000

    Fix styling

--- tests/Http/CategoryManagementControllerTest.php
+++ tests/Http/CategoryManagementControllerTest.php
@@ -10,12 +10,10 @@ class CategoryManagementControllerTest extends TestCase
     /** @test */
     public function it_can_display_list_of_categories()
     {
-    
         Category::create(['name' => 'General']);
         $this->assertDatabaseCount('categories', 1);
 
         Route::category('test');
         $this->get('/test')->assertOk();
-
     }
 }
