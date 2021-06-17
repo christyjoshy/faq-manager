8ab8c7eb5561ad55117ccd1bf3ec08002cb38b76
christy <chisty.joshy@altd.in>
2021-06-17 22:51:49 +0530

    blade layout updated

--- resources/views/Faq/index.blade.php
+++ resources/views/Faq/index.blade.php
@@ -1,19 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.backend')
 
-    <div>
-        @foreach($queries as $query)
-        <h3>{{ $query->category->name }}</h3>
-            <p>{{ $query->question }}</p>
-            <p>{{ $query->answer }}</p>
-        @endforeach
+@section('content')
+<div class="col-md-12">
+    <div class="box-header rounded bg-light text-dark p-4 p-md-5 mb-4 mt-4 d-flex justify-content-center">
+        <h2>List of all Frequently Asked Questions</h2>
     </div>
-</body>
-</html>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="float-right mb-4">
+                        <button class="btn btn-sm btn-primary pull-right">Add New Question</button>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Question</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Updated At</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                @foreach($queries as $query)
+                  <tr>
+                    <th scope="row">{{ $i++ }}</th>
+                    <td width="50%">{{ $query->question }}</td>
+                    <td>{{ $query->category->name }}</td>
+                    <td>{{ $query->updated_at }}</td>
+                    <td width = "20%">
+                        <a class="btn btn-outline btn-warning" href="" rel="nofollow">Edit</a>
+                        <a class="btn btn-outline btn-danger" href="" rel="nofollow">Delete</a>
+                    </td>
+                  </tr>
+                @endforeach
+                </tbody>
+              </table>
+            
+        </div>
+    </div>
+</div>
+@endsection
