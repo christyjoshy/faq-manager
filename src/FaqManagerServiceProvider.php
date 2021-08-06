03167cb73145667fdf9929fabf2e37fdd1c2d018
christy <christy.joshy@altd.in>
2021-08-06 09:47:46 +0100

    conflict resolved 05-08-2021

--- src/FaqManagerServiceProvider.php
+++ src/FaqManagerServiceProvider.php
@@ -26,14 +26,4 @@ class FaqManagerServiceProvider extends PackageServiceProvider
             ->hasCommand(FaqManagerCommand::class);
     }
 
-<<<<<<< HEAD
-=======
-    public function bootingPackage()
-    {
-        Livewire::component('counter', Counter::class);
-        Livewire::component('categories', Categories::class);
-        Livewire::component('faq', Faq::class);
-        Livewire::component('frontendfaq', FrontendFaq::class);
-    }
->>>>>>> c3c046c9cf9f6ba7c966fc47316941d20b0f2080
 }
